e48f1be17b66cd92fd493bac0d57a77e48010cb5
Leonerdos <54115094+jaketverberg@users.noreply.github.com>
2023-08-16 19:50:46 +0100

    CASE and DATE functions

--- notes.sql
+++ notes.sql
@@ -83,6 +83,49 @@ OR This allows you to combine operations where at least one of the combined cond
 JOIN statements allow us to pull data from more than one table at a time.
 Use ON clause to specify a JOIN condition which is a logical statement to combine 
     the table in FROM and JOIN statements.
+JOIN - an INNER JOIN that only pulls data that exists in both tables.
+LEFT JOIN - pulls all the data that exists in both tables, as well as all of the rows 
+    from the table in the FROM even if they do not exist in the JOIN statement.
+RIGHT JOIN - pulls all the data that exists in both tables, as well as all of the rows
+     from the table in the JOIN even if they do not exist in the FROM statement.
 */
+SELECT w.*
+FROM web_events w
+JOIN accounts a
+	ON w.account_id = a.id;
 
 
+A primary key is a unique column in a particular table. This is the first column in each of our tables.
+A foreign key is a column in one table that is a primary key in a different table.
+
+
+When identifying NULLs in a WHERE clause, we write IS NULL or IS NOT NULL. 
+    We don't use =, because NULL isn't considered a value in SQL. Rather, it is a property of the data.
+
+GROUP BY can be used to aggregate data within subsets of the data.
+    Any column in the SELECT statement that is not within an aggregator must be in the GROUP BY clause.
+    The GROUP BY always goes between WHERE and ORDER BY.
+
+DISTINCT is always used in SELECT statements, and it provides the unique rows 
+    for all columns written in the SELECT statement. 
+    Therefore, you only use DISTINCT once in any particular SELECT statement.
+Its worth noting that using DISTINCT, particularly in aggregations, can slow your queries down quite a bit.
+
+
+HAVING is the “clean” way to filter a query that has been aggregated, 
+    but this is also commonly done using a subquery. 
+    Essentially, any time you want to perform a WHERE on an element of your query that was created by an aggregate, 
+    you need to use HAVING instead.
+HAVING appears after the GROUP BY clause but before the ORDER BY clause.
+
+DATE_TRUNC allows you to truncate your date to a particular part of your date-time column. 
+Common trunctions are day, month, and year
+
+
+DATE_PART can be useful for pulling a specific portion of a date, 
+    but notice pulling month or day of the week (dow) means that you are no longer keeping the years in order.
+
+
+CASE statement always goes in the SELECT clause.
+CASE must include the following components: WHEN, THEN, and END. 
+ELSE is an optional component to catch cases that didnt meet any of the other previous CASE conditions.
